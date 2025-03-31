dde38409261f434d4c872f9996975eaaeca8020a
neuma573 <crye@kakao.com>
2025-03-31 14:23:10 +0900

    initial commit

---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/UnifiedBiztoolApplication.java
@@ -0,0 +1,13 @@
+package com.chanwoopark.service.unifiedbiztool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedBiztoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedBiztoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanwoopark/service/unifiedbiztool/UnifiedBiztoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanwoopark.service.unifiedbiztool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedBiztoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
