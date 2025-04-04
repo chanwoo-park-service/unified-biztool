a15825feaec7601c43267e5056fafa7c7eda0e4c
neuma573 <crye@kakao.com>
2025-04-04 17:44:35 +0900

    feat : 엑셀 파싱 이전 검증 메서드 추가

--- src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/controller/MetaController.java
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/controller/MetaController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.List;
+
 @RequestMapping("/api/meta")
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +21,8 @@ public class MetaController {
     private final MetaService metaService;
 
     @PostMapping("/upload")
-    public ResponseEntity<Response<ExcelResponse>> uploadExcel(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<Response<List<ExcelResponse>>> uploadExcel(@RequestParam("file") MultipartFile file) throws IOException {
+        metaService.validateExcel(file);
         return ResponseEntity.ok(
                 Response.of(
                         HttpStatus.OK,
