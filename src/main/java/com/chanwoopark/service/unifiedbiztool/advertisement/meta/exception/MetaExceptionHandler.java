c757ef9c20141d13a603ed10038336310eda40ae
neuma573 <crye@kakao.com>
2025-04-04 17:43:23 +0900

    feat : 예외핸들러에 간단한 로깅 추가

--- src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/exception/MetaExceptionHandler.java
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/exception/MetaExceptionHandler.java
@@ -2,17 +2,20 @@ package com.chanwoopark.service.unifiedbiztool.advertisement.meta.exception;
 
 import com.chanwoopark.service.unifiedbiztool.common.model.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MultipartException;
 
+@Slf4j
 @RestControllerAdvice(basePackages = "com.chanwoopark.service.unifiedbiztool.advertisement.meta")
 public class MetaExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, HttpServletRequest httpServletRequest) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                 ErrorResponse.of(
                         HttpStatus.INTERNAL_SERVER_ERROR,
@@ -24,6 +27,7 @@ public class MetaExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class, MultipartException.class})
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex, HttpServletRequest httpServletRequest) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ErrorResponse.of(
                         HttpStatus.BAD_REQUEST,
