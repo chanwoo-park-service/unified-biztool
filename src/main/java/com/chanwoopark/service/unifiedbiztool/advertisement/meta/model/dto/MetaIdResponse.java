cf6673c26b05f6bf7a8c84a77ca9a341595d1b41
neuma573 <crye@kakao.com>
2025-04-10 16:07:42 +0900

    feat : Meta의 세트, 캠페인, 계정 요청시 응답을 받을 Response Dto 작성

---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/MetaIdResponse.java
@@ -0,0 +1,14 @@
+package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MetaIdResponse {
+    private List<MetaId> data;
+}
