ded4bc4efeb7b6a4adb03526ba220bba6b9000a0
neuma573 <crye@kakao.com>
2025-04-11 22:59:39 +0900

    feat: ValidationMessages.properties 기반 메시지 국제화 설정 추가

---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/AdRequest.java
@@ -0,0 +1,44 @@
+package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.dto;
+
+import com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums.MetaCampaignType;
+import com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums.MetaCreativeFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class AdRequest {
+
+    @NotNull(message = "{adRequest.index.notnull}")
+    private Long index;
+    private MetaCampaignType metaCampaignType;
+    @NotNull(message = "{adRequest.adAccountName.notnull}")
+    private String adAccountName;
+    @NotNull(message = "{adRequest.adAccountId.notnull}")
+    private String adAccountId;
+    @NotNull(message = "{adRequest.campaignName.notnull}")
+    private String campaignName;
+    @NotNull(message = "{adRequest.campaignId.notnull}")
+    private String campaignId;
+    @NotNull(message = "{adRequest.budget.notnull}")
+    private BigDecimal budget;
+    @NotNull(message = "{adRequest.setName.notnull}")
+    private String setName;
+    @NotNull(message = "{adRequest.setId.notnull}")
+    private String setId;
+    @NotNull(message = "{adRequest.adName.notnull}")
+    private String adName;
+    private String shortUrl;
+    private String displayUrl;
+    private String uploadPage;
+    private String memo;
+    @NotNull(message = "{adRequest.code.notnull}")
+    private String code;
+    private String shortUrlFlag;
+    @NotNull(message = "{adRequest.landingUrl.notnull}")
+    private String landingUrl;
+    private String cafe24Url;
+    @NotNull(message = "{adRequest.metaCreativeFormat.notnull}")
+    private MetaCreativeFormat metaCreativeFormat;
+}
