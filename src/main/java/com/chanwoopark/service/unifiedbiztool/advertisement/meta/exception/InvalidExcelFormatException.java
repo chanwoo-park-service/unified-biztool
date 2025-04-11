82490522bab7d350ad085692a02d0595a13bdb87
neuma573 <crye@kakao.com>
2025-04-11 23:02:51 +0900

    feat: InvalidExcelFormatException에 메시지 포맷 파라미터 지원 추가

--- src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/exception/InvalidExcelFormatException.java
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/exception/InvalidExcelFormatException.java
@@ -1,7 +1,20 @@
 package com.chanwoopark.service.unifiedbiztool.advertisement.meta.exception;
 
+import lombok.Getter;
+
+@Getter
 public class InvalidExcelFormatException extends RuntimeException {
+
+    private final Object[] args;
+
     public InvalidExcelFormatException(String message) {
         super(message);
+        this.args = null;
     }
+
+    public InvalidExcelFormatException(String message, Object... args) {
+        super(message);
+        this.args = args;
+    }
+
 }
