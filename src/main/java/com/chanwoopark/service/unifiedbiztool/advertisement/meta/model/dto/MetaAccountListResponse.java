0aa0d851441c1bca10a556e323bfba937de57f14
neuma573 <crye@kakao.com>
2025-04-14 19:00:03 +0900

    feat: Meta API DTO 확장 (Picture, 계정 필드 추가)

---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/MetaAccountListResponse.java
@@ -0,0 +1,12 @@
+package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MetaAccountListResponse {
+    private List<MetaAccountResponse> data;
+}
---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/MetaAccountResponse.java
@@ -0,0 +1,14 @@
+package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MetaAccountResponse {
+    private String name;
+    private String link;
+    private Picture picture;
+    private String id;
+
+}
