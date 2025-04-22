0b3e881cb792f5d81bd30ac2ec867b766758e64d
neuma573 <crye@kakao.com>
2025-04-22 22:36:29 +0900

    feat: 세트 생성 파라미터에 사용하는 입찰전략 enum 정의

---
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/enums/MetaBidStrategy.java
@@ -0,0 +1,8 @@
+package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums;
+
+public enum MetaBidStrategy {
+    OWEST_COST_WITHOUT_CAP,
+    LOWEST_COST_WITH_BID_CAP,
+    COST_CAP,
+    LOWEST_COST_WITH_MIN_ROAS;
+}
