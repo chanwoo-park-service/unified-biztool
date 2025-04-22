54fc42fd1f54fd3e335f3bb12018cab389062fb5
neuma573 <crye@kakao.com>
2025-04-22 22:13:33 +0900

    feat: 클라이언트 요구사항 변경에 따라 campaign type enum 수정

--- src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/enums/MetaCampaignType.java
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/enums/MetaCampaignType.java
@@ -1,27 +1,10 @@
 package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
-
-@RequiredArgsConstructor
 @Getter
 public enum MetaCampaignType {
-    ABO("기본 메타"),
-    ASC("어드밴티지+쇼핑 캠페인"),
-    DEFAULT("");
-
-    private final String description;
-
-    @JsonCreator
-    public static MetaCampaignType from(String value) {
-        if (value == null || value.isBlank()) return DEFAULT;
-
-        return Arrays.stream(values())
-                .filter(type -> type.getDescription().equals(value.trim()))
-                .findFirst()
-                .orElse(DEFAULT);
-    }
+    ABO,
+    ASC,
+    CBO
 }
