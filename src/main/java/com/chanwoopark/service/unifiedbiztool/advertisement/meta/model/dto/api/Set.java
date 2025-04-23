8c1418eb9480bae44adeb1b15d878cf725f8b035
neuma573 <crye@kakao.com>
2025-04-23 11:10:00 +0900

    feat: Set 응답값을 실제 필드에 맞게 수정

--- src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/api/Set.java
+++ src/main/java/com/chanwoopark/service/unifiedbiztool/advertisement/meta/model/dto/api/Set.java
@@ -1,21 +1,39 @@
 package com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.dto.api;
 
 import com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums.MetaAdStatus;
+import com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums.MetaBillingEvent;
+import com.chanwoopark.service.unifiedbiztool.advertisement.meta.model.enums.MetaOptimizationGoal;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Set {
+
     private String id;
+
     private String name;
+
+    @JsonProperty("special_ad_categories")
+    private MetaOptimizationGoal optimizationGoal;
+
+    @JsonProperty("billing_event")
+    private MetaBillingEvent billingEvent;
+
+    @JsonProperty("bid_amount")
+    private Long bidAmount;
+
+    @JsonProperty("campaign_id")
+    private String campaignId;
+
+    private Targeting targeting;
+
+    @JsonProperty("daily_budget")
+    private Long dailyBudget;
+
     private MetaAdStatus status;
-    @JsonProperty("effective_status")
-    private String effectiveStatus;
+
     @JsonProperty("start_time")
     private String startTime;
 }
